c94b2952191774e2fa92aa79bfcc319293874f66
Conner Bryan <conner@clara.com>
2016-09-19 10:34:18 -0700

    update

--- hooks/commands/build.sh
+++ hooks/commands/build.sh
@@ -43,11 +43,13 @@ push_image_to_docker_repository() {
 echo "+++ :docker: Fetching cached docker images"
 
 # see if we are missing any of the images locally, and load them if we are
-local name="${BUILDKITE_PIPELINE_SLUG}_${BUILDKITE_BRANCH}"
-local images_file=s3://$BUILDKITE_IMAGE_CACHE_BUCKET/$name.images
-if aws s3 ls $images_file && ! docker inspect $(aws s3 cp $images_file -) > /dev/null ; then
-    aws s3 cp s3://$BUILDKITE_IMAGE_CACHE_BUCKET/$name.tar.gz - | gunzip -c | docker load
-fi
+(
+  name="${BUILDKITE_PIPELINE_SLUG}_${BUILDKITE_BRANCH}"
+  images_file=s3://$BUILDKITE_IMAGE_CACHE_BUCKET/$name.images
+  if aws s3 ls $images_file && ! docker inspect $(aws s3 cp $images_file -) > /dev/null ; then
+      aws s3 cp s3://$BUILDKITE_IMAGE_CACHE_BUCKET/$name.tar.gz - | gunzip -c | docker load
+  fi
+)
 
 echo "+++ :docker: Building Docker Compose images for service $COMPOSE_SERVICE_NAME"
 
